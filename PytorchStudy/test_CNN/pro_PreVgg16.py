6996d6db36da05dbe88cb62d53ed31bbdf0bf973
XZhiYao <82327193+XZhiYao@users.noreply.github.com>
2021-11-11 16:55:08 +0800

    Update pro_PreVgg16.py

--- PytorchStudy/test_CNN/pro_PreVgg16.py
+++ PytorchStudy/test_CNN/pro_PreVgg16.py
@@ -111,4 +111,47 @@ if __name__ == '__main__':
     print("train sample: ", len(train_data.targets))
     print("val sample: ", len(val_data.targets))
 
-    view_dataset_batch(train_data_loader)
+    # view_dataset_batch(train_data_loader)
+
+    optimizer = torch.optim.Adam(myprevgg.parameters(), lr=0.003)
+    loss_func = nn.CrossEntropyLoss()
+    historyl = hl.History()
+    canvasl = hl.Canvas()
+
+    for epoch in range(10):
+        train_loss_epoch = 0
+        val_loss_epoch = 0
+        train_corrects = 0
+        val_corrects = 0
+
+        myprevgg.train()
+        for step, (b_x, b_y) in enumerate(train_data_loader):
+            output = myprevgg(b_x)
+            loss = loss_func(output, b_y)
+            pre_lab = torch.argmax(output, 1)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+            train_loss_epoch += loss.item() * b_x.size(0)
+            train_corrects += torch.sum(pre_lab == b_y.data)
+
+        train_loss = train_loss_epoch / len(train_data.targets)
+        train_acc = train_corrects.double() / len(train_data.targets)
+
+        myprevgg.eval()
+        for step, (val_x, val_y) in enumerate(val_data_loader):
+            output = myprevgg(val_x)
+            loss = loss_func(output, val_y)
+            pre_lab = torch.argmax(output, 1)
+            val_loss_epoch += loss.item() * val_x.size(0)
+            val_corrects += torch.sum(pre_lab == val_y.data)
+
+        val_loss = val_loss_epoch / len(val_data.targets)
+        val_acc = val_corrects.double() / len(val_data.targets)
+
+        print('train_loss: ', train_loss, 'val_loss: ', val_loss, 'train_acc: ', train_acc.item(), 'val_acc: ', val_acc.item())
+        historyl.log(epoch, train_loss=train_loss, val_loss=val_loss, train_acc=train_acc.item(), val_acc=val_acc.item())
+
+        with canvasl:
+            canvasl.draw_plot([historyl["train_loss"], historyl["val_loss"]])
+            canvasl.draw_plot([historyl["train_acc"], historyl["val_acc"]])
