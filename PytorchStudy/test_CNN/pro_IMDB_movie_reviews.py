82ff661ab4075eae4f5b03655957577f0e1dc09e
XZhiYao <82327193+XZhiYao@users.noreply.github.com>
2021-11-16 02:51:42 +0800

    Update pro_IMDB_movie_reviews.py

--- PytorchStudy/test_CNN/pro_IMDB_movie_reviews.py
+++ PytorchStudy/test_CNN/pro_IMDB_movie_reviews.py
@@ -290,38 +290,38 @@ if __name__ == '__main__':
     print('PAD_IDX: ', PAD_IDX)
     model = CNN_Text(INPUT_DIM, EMBEDDING_DIM, N_FILTERS, FILTER_SIZES, OUTPUT_DIM, DROPOUT, PAD_IDX)
     print(model)
-    #
-    # pretrained_embeddings = TEXT.vocab.vectors
-    # model.embedding.weight.data.copy_(pretrained_embeddings)
-    #
-    # UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
-    # model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
-    # model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
-    #
-    # optimizer = optim.Adam(model.parameters())
-    # criterion = nn.BCEWithLogitsLoss()
-    #
-    # EPOCHS = 10
-    # best_val_loss = float("inf")
-    # best_acc = float(0)
-    # for epoch in range(EPOCHS):
-    #     start_time = time.time()
-    #     train_loss, train_acc = train_epoch(model, train_iter, optimizer, criterion)
-    #     val_loss, val_acc = evaluate(model, val_iter, criterion)
-    #     end_time = time.time()
-    #     print("Epoch: ", epoch+1, "|", "Epoch Time: ", end_time - start_time, "s")
-    #     print("Train Loss: ", train_loss, "|", "Train Acc: ", train_acc)
-    #     print("Val. Loss: ", val_loss, "|", "Val. Acc: ", val_acc)
-    #
-    #     if (val_loss < best_val_loss) & (val_acc > best_acc):
-    #         best_model_wts = copy.deepcopy(model.state_dict())
-    #         best_val_loss = val_loss
-    #         best_acc = val_acc
-    #
-    # model.load_state_dict(best_model_wts)
-    #
-    # test_loss, test_acc = evaluate(model, test_iter, criterion)
-    # print("acc on testing dataset: ", test_acc)
+
+    pretrained_embeddings = TEXT.vocab.vectors
+    model.embedding.weight.data.copy_(pretrained_embeddings)
+
+    UNK_IDX = TEXT.vocab.stoi[TEXT.unk_token]
+    model.embedding.weight.data[UNK_IDX] = torch.zeros(EMBEDDING_DIM)
+    model.embedding.weight.data[PAD_IDX] = torch.zeros(EMBEDDING_DIM)
+
+    optimizer = optim.Adam(model.parameters())
+    criterion = nn.BCEWithLogitsLoss()
+
+    EPOCHS = 10
+    best_val_loss = float("inf")
+    best_acc = float(0)
+    for epoch in range(EPOCHS):
+        start_time = time.time()
+        train_loss, train_acc = train_epoch(model, train_iter, optimizer, criterion)
+        val_loss, val_acc = evaluate(model, val_iter, criterion)
+        end_time = time.time()
+        print("Epoch: ", epoch+1, "|", "Epoch Time: ", end_time - start_time, "s")
+        print("Train Loss: ", train_loss, "|", "Train Acc: ", train_acc)
+        print("Val. Loss: ", val_loss, "|", "Val. Acc: ", val_acc)
+
+        if (val_loss < best_val_loss) & (val_acc > best_acc):
+            best_model_wts = copy.deepcopy(model.state_dict())
+            best_val_loss = val_loss
+            best_acc = val_acc
+
+    model.load_state_dict(best_model_wts)
+
+    test_loss, test_acc = evaluate(model, test_iter, criterion)
+    print("acc on testing dataset: ", test_acc)
 
 
 
