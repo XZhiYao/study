dc66e0363efe64992c69c24944dfd8029c39b7e5
XZhiYao <82327193+XZhiYao@users.noreply.github.com>
2021-11-16 17:16:49 +0800

    Update test_Single_Img.py

--- PytorchStudy/test_CNN/test_Single_Img.py
+++ PytorchStudy/test_CNN/test_Single_Img.py
@@ -11,9 +11,36 @@ from torchvision import transforms
 from PIL import Image
 
 
+class MyVgg16(nn.Module):
+    def __init__(self):
+        super(MyVgg16, self).__init__()
+        self.vgg = models.vgg16(pretrained=True)
+        self.features_conv = self.vgg.features[:30]
+        self.max_pool = self.vgg.features[30]
+        self.avgpool = self.vgg.avgpool
+        self.classifier = self.vgg.classifier
+        self.gradients = None
+
+    def activation_hook(self, grad):
+        self.gradients = grad
+
+    def forward(self, x):
+        x = self.features_conv(x)
+        h = x.register_hook(self.activation_hook)
+        x = self.max_pool(x)
+        x = self.avgpool(x)
+        x = x.view((1, -1))
+        x = self.classifier(x)
+        return x
+
+    def get_activations_gradient(self):
+        return self.gradients
+
+    def get_activations(self, x):
+        return self.features_conv(x)
+
+
 activation = {}
-
-
 def get_activation(name):
     def hook(model, input, output):
         activation[name] = output.detach()
@@ -74,5 +101,73 @@ if __name__ == '__main__':
 
     # view_superfical_featureMap(vgg16)
     # view_deep_featureMap(vgg16)
+
+    # LABELS_URL = "https://gist.github.com/yrevar/942d3a0ac09ec9e5eb3a.js"
+    # response = requests.get(LABELS_URL)
+    # labels = {int(key): value for key, value in response.json().item()}
+    LABELS_DIR = "../DataFrame/imagenet1000_clsidx_to_labels.txt"
+    labels = {}
+    with open(LABELS_DIR, "r") as f:
+        for data in f.readlines():
+            data = data.strip('{')
+            data = data.strip('}')
+            data = data.strip('\n')
+            (key, value) = data.strip().split(':')
+            labels[int(key)] = value.replace("'", '')
+
+    vgg16.eval()
+    im_pre = vgg16(input_im)
+    softmax = nn.Softmax(dim=1)
+    im_pre_prob = softmax(im_pre)
+    prob, prelab = torch.topk(im_pre_prob, 5)
+    prob = prob.data.numpy().flatten()
+    prelab = prelab.numpy().flatten()
+    for ii, lab in enumerate(prelab):
+        print("index: ", lab, "label: ", labels[lab], "||", prob[ii])
+
+    print('--------------------------------------------------------------------------------------------')
+    vggcam = MyVgg16()
+    vggcam.eval()
+    im_pre = vggcam(input_im)
+    softmax = nn.Softmax(dim=1)
+    im_pre_prob = softmax(im_pre)
+    prob, prelab = torch.topk(im_pre_prob, 5)
+    prob = prob.data.numpy().flatten()
+    prelab = prelab.numpy().flatten()
+    for ii, lab in enumerate(prelab):
+        # print(ii)
+        print("index: ", lab, "label: ", labels[lab], "||", prob[ii])
+
+    # print('--------------------------------------------------------------------------------------------')
+    im_pre[:, prelab[0]].backward()
+    gradients = vggcam.get_activations_gradient()
+    mean_gradients = torch.mean(gradients, dim=[0, 2, 3])
+    activations = vggcam.get_activations(input_im).detach()
+    for i in range(len(mean_gradients)):
+        activations[:, i, :, :] *= mean_gradients[i]
+    heatmap = torch.mean(activations, dim=1).squeeze()
+    heatmap = F.relu(heatmap)
+    heatmap /= torch.max(heatmap)
+    heatmap = heatmap.numpy()
+    plt.matshow(heatmap)
+    # plt.show()
+
+    img = cv2.imread("../Dataset/image.jpg")
+    heatmap = cv2.resize(heatmap, (img.shape[1], img.shape[0]))
+    heatmap = np.uint8(255 * heatmap)
+    heatmap = cv2.applyColorMap(heatmap, cv2.COLORMAP_JET)
+    Grad_cam_img = heatmap * 0.4 + img
+    Grad_cam_img = Grad_cam_img / Grad_cam_img.max()
+
+    b, g, r = cv2.split(Grad_cam_img)
+    Grad_cam_img = cv2.merge([r, g, b])
+    plt.figure()
+    plt.imshow(Grad_cam_img)
+    plt.show()
+
+
+
+
+
 
 
